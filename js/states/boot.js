44a8bb010ce1477ba1410d82e6575f7c8a44db78
hrh <huangrihua@hqyatu.com>
2017-12-14 09:55:43 +0800

    Phaser快速入门

--- js/states/boot.js
+++ js/states/boot.js
@@ -2,30 +2,29 @@
 var Boot = function(game) {
     var baseURI = localStorage.baseURI
     this.init = function() {
-        console.log('111111');
         //game.device.desktop判断是WAP端还是PC端，true为PC端，false为WAP端
         if (!game.device.desktop) {
             //设置游戏背景色
-            game.stage.backgroundColor = '#282C34';
+            game.stage.backgroundColor = '#282C34'
             //鼠标指针对象，由于WAP端没有鼠标，因此设置为1（即为null）
-            game.input.maxPointers = 1;
+            game.input.maxPointers = 1
             //缩放控制，这里将画布（canvas）拉伸至填满父容器（即#game-container），不保持比例
-            game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL
             //启用时，显示画布（canvas）将在父容器中水平对齐
-            game.scale.pageAlignHorizontally = true;
+            game.scale.pageAlignHorizontally = true
             //启用时，显示画布（canvas）将在父容器中垂直对齐
-            game.scale.pageAlignVertically = true;
+            game.scale.pageAlignVertically = true
             //强制游戏只能以一个方向运行，这里设置游戏仅能在纵向模式运行（true），无法再横向模式运行（false）
-            game.scale.forceOrientation(false, true);
+            game.scale.forceOrientation(false, true)
             //当forceOrientation设置只能纵向模式运行，把手机横向摆放就会调用enterIncorrectOrientation这个方法
-            game.scale.enterIncorrectOrientation.add(enterIncorrectOrientation, this);
+            game.scale.enterIncorrectOrientation.add(enterIncorrectOrientation, this)
             //当forceOrientation设置只能纵向模式运行，把手机横向摆放就会调用enterIncorrectOrientation这个方法，重新将手机纵向摆放就会调用leaveIncorrectOrientation这个方法
-            game.scale.leaveIncorrectOrientation.add(leaveIncorrectOrientation, this);
+            game.scale.leaveIncorrectOrientation.add(leaveIncorrectOrientation, this)
         }else{
             game.scale.pageAlignHorizontally = true
             game.scale.pageAlignVertically = true
             // 防止浏览器失去焦点后动画暂停，如果考虑到计算机性能可以设置为false
-            game.stage.disableVisibilityChange = true;
+            game.stage.disableVisibilityChange = true
         }
 
     }
@@ -38,15 +37,15 @@ var Boot = function(game) {
     }
     this.create = function() {
         //跳转到下一个场景
-        game.state.start('Preload');
+        game.state.start('Preload')
     }
 
     function enterIncorrectOrientation() {
-        alert('请将手机纵向摆放');
+        alert('请将手机纵向摆放')
     }
 
     function leaveIncorrectOrientation() {
-        alert('已经将手机纵向摆放');
+        alert('已经将手机纵向摆放')
     }
 
 }
